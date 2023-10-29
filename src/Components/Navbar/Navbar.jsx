acd5449d42e1aee48481c4f3f7f9b59a57809a76
Your Name <rownakhayat@gmail.com>
2023-10-30 00:18:26 +0600

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assest/css/Navbar.css'
+import navbg from '../assest/images/bagImage/Rectangle 17.png'
+const Navbar = () => {
+  return (
+    <div className="h-screen bg-blue-500" style={{ backgroundImage: `url(${navbg})` }} >
+            navbar
+    </div>
+  )
+}
+
+export default Navbar
